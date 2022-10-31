d8f3689fdd5b192d625e0cc58eb4495731b87068
strongcookdas <goodjav141592@gmail.com>
2022-10-31 17:59:23 +0900

    start : seoul_hospital_data_parsing_project

---
+++ src/test/java/com/example/mini_project_connect_seoul_hospital_data/MiniProjectConnectSeoulHospitalDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_connect_seoul_hospital_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectConnectSeoulHospitalDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
